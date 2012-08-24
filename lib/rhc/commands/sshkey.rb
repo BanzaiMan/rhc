a2e4513968784bce20424c201f9e54b3d52b4e12
Hiro Asari <asari.ruby@gmail.com>
2012-08-24 16:50:23 -0400

    Simplify 'rhc sshkey list' output

--- lib/rhc/commands/sshkey.rb
+++ lib/rhc/commands/sshkey.rb
@@ -1,6 +1,8 @@
 #! /usr/bin/env ruby
 
 require 'rhc/coverage_helper'
+require 'rhc/commands/base'
+require 'rhc/config'
 require 'rhc/vendor/sshkey'
 
 module RHC::Commands
@@ -16,28 +18,18 @@ module RHC::Commands
     def list
       ssh_keys = RHC::get_ssh_keys(RHC::Config[libra_server], options.rhlogin, options.password, RHC::Config[default_proxy])
       additional_ssh_keys = ssh_keys['keys']
-  
-      puts ""
-      puts "SSH keys"
-      puts "========"
-
-      # first list the primary key
-      puts "       Name: default"
-      puts "       Type: #{ssh_keys['ssh_type']}"
-      puts "Fingerprint: #{ssh_keys['fingerprint']}"
-      #puts "        Key: #{ssh_keys['ssh_key']}"
-      puts ""
-    
-      # now list the additional keys
-      if additional_ssh_keys && additional_ssh_keys.kind_of?(Hash)
-        additional_ssh_keys.each do |name, keyval|
-          puts "       Name: #{name}"
-          puts "       Type: #{keyval['type']}"
-          puts "Fingerprint: #{keyval['fingerprint']}"
-          #puts "        Key: #{keyval['key']}"
-          puts ""
-        end
+      
+      keys = []
+      # top-level key will be named 'default'
+      keys << SSHPublicKey.new(ssh_keys)
+      additional_ssh_keys.each do |k, v|
+        keys << SSHPublicKey.new('name' => k, 'ssh_type' => v['type'], 'fingerprint' => v['fingerprint'])
       end
+
+      puts "\nSSH keys\n========"
+      puts keys.map {|k| k.to_s}
+      
+      true
     end
 
     summary 'Add SSH key to the user account'
@@ -60,6 +52,27 @@ module RHC::Commands
     def remove
       data = {:rhlogin => options.rhlogin, :key_name => options.identifier, :action => 'remove-key'}
       handle_key_mgmt_response URI.parse("https://#{RHC::Config[libra_server]}/broker/ssh_keys"), data, options.password      
+    end
+    
+    private
+    ## given a hash with keys 'name', 'ssh_type' and 'fingerprint', treat it
+    ## as though it is a representation of an SSH public key.
+    ## We can't use RHC::Vendor::SSHKey here, since that requires access to
+    ## a private key, which we don't have
+    class SSHPublicKey
+      attr_reader :name, :type, :finger_print
+      def initialize(data)
+        @name = data['name'] || 'default'
+        @type = data['ssh_type']
+        @finger_print = data['fingerprint']
+      end
+      
+      def to_s
+        puts "       Name: #{name}"
+        puts "       Type: #{type}"
+        puts "Fingerprint: #{finger_print}"
+        puts ""
+      end
     end
 
   end
