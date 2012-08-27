2ba09514afd2fa4e12f1e635cd9a91d463d62183
Hiro Asari <asari.ruby@gmail.com>
2012-08-27 17:18:45 -0400

    Revert "Refactor stubs for user and key requests to private methods."
    This reverts commit 8e81dead39ee3e68f721af1742b26ebdacbd5698.

--- spec/rhc/rest_client_spec.rb
+++ spec/rhc/rest_client_spec.rb
@@ -307,7 +307,16 @@ module RHC
 
         context "#user" do
           before(:each) do
-            stub_user_request
+            stub_api_request(:any, client_links['GET_USER']['relative']).
+              to_return({ :body   => {
+                            :type => 'user',
+                            :data =>
+                            { :login => mock_user,
+                              :links => mock_response_links(mock_user_links)
+                            }
+                          }.to_json,
+                          :status => 200
+                        })
           end
           it "returns the user object associated with this client connection" do
             user = @client.user
@@ -319,8 +328,33 @@ module RHC
 
         context "#find_key" do
           before(:each) do
-            stub_user_request
-            stub_keys_request
+            stub_api_request(:any, client_links['GET_USER']['relative']).
+              to_return({ :body   => {
+                            :type => 'user',
+                            :data =>
+                            { :login => mock_user,
+                              :links => mock_response_links(mock_user_links)
+                            }
+                          }.to_json,
+                          :status => 200
+                        })
+            stub_api_request(:any, user_links['LIST_KEYS']['relative']).
+              to_return({ :body   => {
+                            :type => 'keys',
+                            :data =>
+                            [{ :name    => 'mock_key_0',
+                               :type    => 'mock_key_0_type',
+                               :content => '123456789:0',
+                               :links   => mock_response_links(mock_key_links('mock_key_0'))
+                             },
+                             { :name    => 'mock_key_1',
+                               :type    => 'mock_key_1_type',
+                               :content => '123456789:1',
+                               :links   => mock_response_links(mock_key_links('mock_key_1'))
+                             }]
+                          }.to_json,
+                          :status => 200
+                        })
           end
           it "returns a list of key objects for matching keys" do
             key = nil
@@ -397,41 +431,6 @@ module RHC
           it_should_behave_like "a logout method"
         end
       end
-    end
-  
-    private
-    def stub_user_request
-      stub_api_request(:any, client_links['GET_USER']['relative']).
-        to_return({ :body   => {
-                      :type => 'user',
-                      :data =>
-                      { :login => mock_user,
-                        :links => mock_response_links(mock_user_links)
-                      }
-                    }.to_json,
-                    :status => 200
-                  })
-    end
-    
-    def stub_keys_request
-      stub_api_request(:any, user_links['LIST_KEYS']['relative']).
-        to_return({ :body   => {
-                      :type => 'keys',
-                      :data =>
-                      [{ :name    => 'mock_key_0',
-                         :type    => 'mock_key_0_type',
-                         :content => '123456789:0',
-                         :links   => mock_response_links(mock_key_links('mock_key_0'))
-                       },
-                       { :name    => 'mock_key_1',
-                         :type    => 'mock_key_1_type',
-                         :content => '123456789:1',
-                         :links   => mock_response_links(mock_key_links('mock_key_1'))
-                       }]
-                    }.to_json,
-                    :status => 200
-                  })
-      
     end
   end
 end
